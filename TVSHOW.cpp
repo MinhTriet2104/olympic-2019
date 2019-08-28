9d086ff6ec55eed0dfb5ee2359ef299d4086bd86
MinhTriet2104 <45585341+MinhTriet2104@users.noreply.github.com>
2019-08-28 14:02:12 +0700

    Bài 4 CD 2011 (Sửa lần 3)

--- TVSHOW.cpp
+++ TVSHOW.cpp
@@ -1,6 +1,5 @@
 #include <fstream>
-#include <vector>
-//#include <algorithm>
+#include <time.h>
 //#include <iostream>
 using namespace std;
 
@@ -9,25 +8,27 @@ struct Time {
 	int end;
 };
 
-//bool compare(Time t1, Time t2) {
-//	return t1.start < t2.start;
-//}
+int compare(const void * a, const void * b) {
+	Time *a1 = (Time*)a;
+	Time *b1 = (Time*)b;
+	return a1->start < b1->start;
+}
 
 int main() {
+	clock_t start = clock();
 	ifstream docFile;
 	docFile.open("TVSHOW.INP");
 
 	int n;
 	docFile >> n;
 
-	vector<Time> arr(n);
+	Time arr[50000];
 	for (int i = 0; i < n; i++) {
 		docFile >>  arr[i].start;
 		docFile >> arr[i].end;
 	}
 	docFile.close();
 
-	//sort(arr.begin(), arr.end(), compare);
 
 	//for (int i = 0; i < n; i++) {
 	//	cout <<  arr[i].start;
@@ -35,21 +36,33 @@ int main() {
 	//	cout << arr[i].end << endl;
 	//}
 
+	qsort(arr, n, sizeof(Time), compare);
 	int count = 0;
 	for (int i = 0; i < n - 1; i++) {
 		for (int j = i + 1; j < n; j++) {
-			if (arr[i].start >= arr[j].start && arr[i].start <= arr[j].end || arr[j].start >= arr[i].start && arr[j].start <= arr[i].end) {
-				//cout << arr[i].start << " " << arr[i].end << " " << arr[j].start << " " << arr[j].end << endl;
+			if (arr[j].start <= arr[i].end) {
+				/*cout << arr[i].start << " " << arr[i].end << " " << arr[j].start << " " << arr[j].end << endl;*/
 				count++;
 			}
 		}
 	}
 	
+	//int count = 0;
+	//for (int i = 0; i < n - 1; i++) {
+	//	for (int j = i + 1; j < n; j++) {
+	//		if (arr[i].start >= arr[j].start && arr[i].start <= arr[j].end || arr[j].start >= arr[i].start && arr[j].start <= arr[i].end) {
+	//			//cout << arr[i].start << " " << arr[i].end << " " << arr[j].start << " " << arr[j].end << endl;
+	//			count++;
+	//		}
+	//	}
+	//}
+
 	ofstream ghiFile;
 	ghiFile.open("TVSHOW.OUT");
 	ghiFile << count;
 	ghiFile.close();
 
-	//system("pause");
+	printf("Time: %.2fs\n", (double)(clock() - start)/CLOCKS_PER_SEC);
+	system("pause");
 	return 0;
 }
