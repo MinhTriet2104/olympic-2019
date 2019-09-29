9c8b3df8e669366a1ab94c86d40fc19ef4ecd69d
MinhTriet2104 <45585341+MinhTriet2104@users.noreply.github.com>
2019-09-29 15:05:26 +0700

    CD 2013 (29/09/2019) fix

--- FSEQ.cpp
+++ FSEQ.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <iostream>
 #include <vector>
 using namespace std;
 
@@ -11,29 +12,29 @@ int main() {
 	docFile >> n;
 
 	vector<int> a1(n);
-	vector<int> a2(n, 0);
 	for (int i = 0; i < n; i++) {
 		docFile >> a1[i];
 	}
-	
+
+	int count = 0;
+	int max = 0;
 	for (int i = 2; i < n; i++) {
 		if (a1[i] == a1[i - 1] + a1[i - 2]) {
-			if (a2[i - 1] == 0) {
-				a2[i] = 3;
-			} else {
-				a2[i] = a2[i - 1] + 1;
+			count++;
+		}
+		else {
+			if (count > max) {
+				max = count;
 			}
+			count = 0;
 		}
 	}
-
-	int max = a2[2];
-	for (int i = 3; i < n; i++) {
-		if (a2[i] > max) max = a2[i];
+	if (count > max) {
+		max = count;
 	}
-
 	ghiFile.open("FSEQ.OUT");
 	if (max == 0) ghiFile << -1;
-	else ghiFile << max;
+	else ghiFile << max + 2;
 
 	return 0;
 }
